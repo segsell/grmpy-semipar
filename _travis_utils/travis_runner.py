9c0e347e89a8221c0d15dee93acc215fcf403331
Sebastian Gsell <sebastiangsell@Sebastians-MBP.fritz.box>
2019-09-13 15:40:41 +0200

    Trying Travis CI

---
+++ _travis_utils/travis_runner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""This script manages all tasks for the TRAVIS build server."""
+import subprocess as sp
+
+if __name__ == "__main__":
+
+    notebook = "replication-Carneiro2011.ipynb"
+    cmd = " jupyter nbconvert --execute {}  \
+    --ExecutePreprocessor.timeout=-1".format(
+        notebook
+    )
+    sp.check_call(cmd, shell=True)
