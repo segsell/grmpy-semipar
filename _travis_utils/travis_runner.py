3cb03df524ac1d655b1e16a830d1b46c558a42fe
Sebastian Gsell <sebastian.gsell93@gmail.com>
2020-01-18 10:42:53 +0100

    Handling security issue in travis_runner.

--- _travis_utils/travis_runner.py
+++ _travis_utils/travis_runner.py
@@ -2,11 +2,22 @@
 """This script manages all tasks for the TRAVIS build server."""
 import subprocess as sp
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
+#
+#     notebook = "thesis_notebook.ipynb"
+#     cmd = " jupyter nbconvert --execute {}  \
+#     --ExecutePreprocessor.timeout=-1".format(
+#         notebook
+#     )
+#     sp.check_call(cmd, shell=True)
 
-    notebook = "thesis_notebook.ipynb"
-    cmd = " jupyter nbconvert --execute {}  \
-    --ExecutePreprocessor.timeout=-1".format(
-        notebook
-    )
-    sp.check_call(cmd, shell=True)
+
+if __name__ == "__main__":
+    cmd = [
+        "jupyter",
+        "nbconvert",
+        "--execute",
+        "thesis_notebook.ipynb",
+        "--ExecutePreprocessor.timeout=-1",
+    ]
+    sp.check_call(cmd)
