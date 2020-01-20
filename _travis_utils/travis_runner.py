c27774b2ffd2c71054706611290394e0a5e2af98
Sebastian Gsell <sebastian.gsell93@gmail.com>
2020-01-20 12:27:23 +0100

    Noebook does not execute when moved to subfolder.

--- _travis_utils/travis_runner.py
+++ _travis_utils/travis_runner.py
@@ -1,26 +1,13 @@
 #!/usr/bin/env python
 """This script manages all tasks for the TRAVIS build server."""
-import os
 import subprocess as sp
 
-# if __name__ == "__main__":
-#     cmd = [
-#         "jupyter",
-#         "nbconvert",
-#         "--execute",
-#         "thesis_notebook.ipynb",
-#         "--ExecutePreprocessor.timeout=-1",
-#     ]
-#     sp.check_call(cmd)
-
 if __name__ == "__main__":
-    os.chdir("promotion/thesis_notebook")
     cmd = [
         "jupyter",
         "nbconvert",
         "--execute",
-        "thesis_notebook_copy.ipynb",
+        "thesis_notebook.ipynb",
         "--ExecutePreprocessor.timeout=-1",
     ]
     sp.check_call(cmd)
-    os.chdir("../..")
